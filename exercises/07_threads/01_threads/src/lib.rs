c7abf12b6965f787d62af9759cd8be64775ea594
Michael Smith <m.steve.smith@gmail.com>
2025-06-10 09:13:27 -0400

    updates

--- exercises/07_threads/01_threads/src/lib.rs
+++ exercises/07_threads/01_threads/src/lib.rs
@@ -15,7 +15,21 @@
 use std::thread;
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    //todo!()
+    let middle = v.len() / 2;
+    let (left, right) = v.split_at(middle);
+
+    let left = left.to_vec();
+    let right = right.to_vec();
+
+    let handle_left = thread::spawn(move || {
+        left.iter().sum::<i32>()
+    });
+    let handle_right = thread::spawn(move || {
+        right.iter().sum::<i32>()
+    });
+
+    handle_left.join().unwrap() + handle_right.join().unwrap()
 }
 
 #[cfg(test)]
